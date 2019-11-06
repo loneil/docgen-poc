757edd425b0544394941dc32afaf5454ec14e017
Lucas O'Neil <lucasoneil@gmail.com>
2019-11-06 13:00:18 -0800

    Work in progress

--- app/src/components/docGen.js
+++ app/src/components/docGen.js
@@ -8,8 +8,8 @@ const docGen = {
    *  @param {object} context The object of replacement variables
    *  @param {object} response The server response to write the generated file to
    */
-  generateDocument: async (context, response) => {
-    carbone.render('./node_modules/carbone/examples/simple.odt', context, function (err, result) {
+  generateDocument: async (body, response) => {
+    carbone.render('./node_modules/carbone/examples/simple.odt', body.context, function (err, result) {
       if (err) {
         log(`Error during Carbone generation. Error: ${err}`);
         throw new Error(err);
--- app/tests/unit/components/validators/validators.customValidators.spec.js
+++ app/tests/unit/components/validators/validators.customValidators.spec.js
@@ -13,21 +13,26 @@ describe('customValidators.docGen', () => {
         x: 1,
         y: 2
       },
+      template: {
+        content: 'ZHNmc2Rmc2RmZHNmc2Rmc2Rmc2Rm',
+        contentEncodingType: 'base64'
+      }
     };
   });
 
-  it('should return an empty error array when valid', () => {
-    const result = customValidators.docGen(body);
+  it('should return an empty error array when valid', async () => {
+    const result = await customValidators.docGen(body);
+    console.log(result);
 
     expect(result).toBeTruthy();
     expect(Array.isArray(result)).toBeTruthy();
     expect(result.length).toEqual(0);
   });
 
-  it('should return an error with validation error when invalid', () => {
+  it('should return an error with validation error when invalid', async () => {
     body.context = 'garbage';
 
-    const result = customValidators.docGen(body);
+    const result = await customValidators.docGen(body);
 
     expect(result).toBeTruthy();
     expect(Array.isArray(result)).toBeTruthy();
